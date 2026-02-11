cb2cfcd351b6a3d71038c596fc69fba90094812f
Josue777h <josuexdsepulvedajaja@gmail.com>
2026-02-10 22:33:47 -0500

    sw

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,1 +1,27 @@
+/* eslint-disable no-undef */
+importScripts(
+  "https://www.gstatic.com/firebasejs/10.12.4/firebase-app-compat.js",
+);
+importScripts(
+  "https://www.gstatic.com/firebasejs/10.12.4/firebase-messaging-compat.js",
+);
 
+firebase.initializeApp({
+  apiKey: "TU_API_KEY",
+  authDomain: "listadetareas-cb9a7.firebaseapp.com",
+  projectId: "listadetareas-cb9a7",
+  storageBucket: "listadetareas-cb9a7.firebasestorage.app",
+  messagingSenderId: "246655635442",
+  appId: "TU_APP_ID",
+});
+
+const messaging = firebase.messaging();
+
+messaging.onBackgroundMessage((payload) => {
+  const title = payload.notification?.title || "FinanzasApp";
+  const options = {
+    body: payload.notification?.body || "",
+    icon: "/logo192.png",
+  };
+  self.registration.showNotification(title, options);
+});
